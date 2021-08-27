6b54c8be8eb432a91c3c2089de827197012c5e29
IlyaK <ilja.kasatckin@yandex.ru>
2021-08-27 20:21:10 +0300

    Добавил сервлет и tomcat

--- src/main/java/payback/database/DataBase.java
+++ src/main/java/payback/database/DataBase.java
@@ -14,21 +14,23 @@ public class DataBase {
         URI dbUri = new URI(System.getenv("postgres://gyqdavjdmxqlmm:f356e0e1aa2bd94f9487778199133bc0de1efe1c5313b7700d4f4374b33e4100@ec2-54-154-101-45.eu-west-1.compute.amazonaws.com:5432/dbm0srl4egvboe"));
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
         connection = DriverManager.getConnection(dbUrl, username, password);
         statement = connection.createStatement();
     }
 
-    private void checkTableParties() throws SQLException {
+    public void checkTableParties() throws SQLException {
         String createDatabase = "Create Table Test(id int primary key, name varchar, address text) ";
         statement.executeUpdate(createDatabase);
     }
 
     public boolean createParty(String nameParty, String codeParty, String startTimeParty, String endTimeParty) throws SQLException {
-        checkTableParties();
+        //checkTableParties();
         /*String insertParty = String.format("INSERT INTO parties(name_party, code_party, date_start_party, date_end_party)\n" +
-                "VALUES (%s, %s, %s, %s);", nameParty, codeParty, startTimeParty, endTimeParty);
-        statement.executeUpdate(insertParty);*/
+                "VALUES (%s, %s, %s, %s);", nameParty, codeParty, startTimeParty, endTimeParty);*/
+        String insertParty = String.format("INSERT INTO parties(party_id)\n" +
+                "VALUES (%d);", 8);
+        statement.executeUpdate(insertParty);
         return true;
     }
 
