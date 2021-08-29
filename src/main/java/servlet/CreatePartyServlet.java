45a09ccdd03be94dba6bf6d720f821efb7b810bc
IlyaK <ilja.kasatckin@yandex.ru>
2021-08-29 18:23:03 +0300

    change servlet

--- src/main/java/servlet/CreatePartyServlet.java
+++ src/main/java/servlet/CreatePartyServlet.java
@@ -1,52 +1,27 @@
 package servlet;
 
-import org.json.JSONObject;
-import payback.Party;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URISyntaxException;
-import java.sql.SQLException;
-import java.util.HashMap;
 
-@WebServlet("/createparty")
+@WebServlet(
+        name = "MyServlet",
+        urlPatterns = {"/hello"}
+)
 public class CreatePartyServlet extends HttpServlet {
-    Party party = new Party();
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //parametersLogger();
-
-        System.out.println("In do Get");
-        System.out.println(request.getParameter("TAG"));
-        //JSONObject jsonRequest = new JSONObject(request.getParameter("TAG"));//from url in app
-            /*System.out.println(jsonRequest);
-            party.setNameParty(jsonRequest.getString("name_party"));
-            party.setDateStart(jsonRequest.getString("data_start"));
-            party.setDateEnd(jsonRequest.getString("data_end"));
-            party.generateCodeParty();*/
-
-        System.out.println("PartyName: " + party.getNameParty());
-
-        //send back to app
-        HashMap<String, String> map;
-        map = new HashMap<>();
-        map.put("code_party", party.getCodeParty());
-
-        JSONObject replyJSON = new JSONObject(map);
-        response.setContentType("application/json");
-        PrintWriter printW = response.getWriter();
-        printW.println(replyJSON);
-        //party.createInDataBase();
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hello heroku".getBytes());
+        out.flush();
+        out.close();
     }
 }
+
