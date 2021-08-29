0c99837ca2695f1d70794cc78c0fff9b9ff39e4d
IlyaK <ilja.kasatckin@yandex.ru>
2021-08-29 19:09:08 +0300

    change servlet add jsp

--- src/main/java/servlet/CreatePartyServlet.java
+++ src/main/java/servlet/CreatePartyServlet.java
@@ -1,26 +1,43 @@
 package servlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import payback.Party;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 
 @WebServlet(
         name = "MyServlet",
         urlPatterns = {"/party_create"}
 )
 public class CreatePartyServlet extends HttpServlet {
-
+    Party party;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
-        out.flush();
-        out.close();
+        String request = req.getParameter("TAG");
+        JSONObject json = new JSONObject(request);
+        party = new Party();
+        party.setNameParty((String)json.getJSONArray("name_party").get(0));
+        party.generateCodeParty();
+        JSONArray jsonDataStart = json.getJSONArray("data_start");
+        party.setDateStart(jsonDataStart.get(0) + " " + jsonDataStart.get(1));
+        JSONArray jsonDataEnd = json.getJSONArray("data_end");
+        party.setDateEnd(jsonDataEnd.get(0) + " " + jsonDataEnd.get(1));
+
+        resp.setContentType("application/json");
+        HashMap<String, String> map;
+        map = new HashMap<>();
+        map.put("code_party",party.getCodeParty());
+        JSONObject replyJSON = new JSONObject(map);
+        PrintWriter printW = resp.getWriter();
+        printW.println(replyJSON);
     }
 
     @Override
