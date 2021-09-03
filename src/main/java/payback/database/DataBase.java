ec75afd9cc2682b4d3e78aac82c5f3b90424ebd9
i.kasatkin <ilja.kasatckin@yandex.ru>
2021-09-03 15:01:09 +0300

    Добавил два способа вставки мероприятия

--- src/main/java/payback/database/DataBase.java
+++ src/main/java/payback/database/DataBase.java
@@ -43,7 +43,7 @@ public class DataBase {
         connect();
         LOGGER.info(String.format("Создание мероприятия %s в базе данных", nameParty));
         String insertParty;
-        if (startTimeParty == null ){
+        if (startTimeParty == null || endTimeParty == null){
             insertParty = String.format("INSERT INTO public.Parties" +
                             " (NameParty, CodeParty) VALUES ('%s', '%s')", nameParty,
                     codeParty);
